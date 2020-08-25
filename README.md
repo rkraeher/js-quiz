a424461e71646a30c2c3c4df0d4c92277bab3782
Ru <rudikraeher@gmail.com>
2020-08-25 15:23:53 -0700

    created quiz welcome page and styles; started quiz question and answer generator functions; built first few question and answer objects

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # js-quiz
 A timed quiz testing a new student's knowledge of JavaScript fundamentals
+
+// # Acceptance Criteria
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+// #Pseudo Code
+// I want a welcome page that describes the quiz
+// The welcome page has a start quiz button that begins the quiz
+// Pressing the start button also begins a timer that counts down
+// There are multiple choice and true/false questions with multiple buttons for the user to press to answer
+// There is a score counter that keeps track of every correct answer
+// If a user answers a question incorrectly, time is subtracted from the timer as a penalty
+// Either when the user answers all questions OR the timer reaches 0, the game ends
+// At the end of the game, a page is displayed with the final score and an input for user initials
+// The user intials and score are saved in local storage and displayed as the last saved input (View High Scores)
+// There is a button to restart the quiz.
--- js/script.js
+++ js/script.js
@@ -1,27 +1,96 @@
+var score = 0;
+var timer = 0;
+var totalTime = 60; 
+var interval;
+
+var timeDisplay = document.querySelector("#seconds");
+var startButton = document.querySelector("#start");
+var questionText = document.querySelector("h1");
+var answers = document.querySelector(".jumbotron");
+var lead = document.querySelector(".lead");
+var lead2 = document.querySelector(".lead2");
+
+// HTML Question Objects
+var voidElement = {
+    question: "Which of the following HTML elements is not a void element and needs a closing tag?",
+    correct: "Anchor ",
+    wrong: ["Img", "Break", "Input"]
+};
+
+var headings = {
+    question: "How many types of HTML headings are there?",
+    correct: 6,
+    wrong: [3, 5, 7]
+};
+
+// CSS Question Objects
+var positionStates = {
+    question: "Which of the following is not a CSS position state?",
+    correct: "flex",
+    wrong: ["relative", "absolute", "fixed"]
+}
+
+startButton.addEventListener("click", quiz);       
+
+function quiz (){      //TODO Fill in the function parameters with a placeholder, so that where "voidElement" is now can be interchangeable. 
+    questionText.textContent = voidElement.question;
+    lead.textContent = "";
+    lead2.textContent = "";
+    startButton.parentNode.removeChild(startButton);
+    correctButton();
+    
+};
+
+function correctButton () {       // If I set the parameters to something, that something can also go where voidElement is to make this more reusable. 
+    var answerBtn = document.createElement("button");
+    answerBtn.innerHTML = voidElement.correct;
+    answerBtn.setAttribute("class", "btn btn-info btn-lg m-2");
+    answers.appendChild(answerBtn);
+};
 
 
 
-// # Acceptance Criteria
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
 
-// #Pseudo Code
-// I want a welcome page that describes the quiz
-// The welcome page has a start quiz button that begins the quiz
-// Pressing the start button also begins a timer that counts down
-// There are multiple choice and true/false questions with multiple buttons for the user to press to answer
-// There is a score counter that keeps track of every correct answer
-// If a user answers a question incorrectly, time is subtracted from the timer as a penalty
-// Either when the user answers all questions OR the timer reaches 0, the game ends
-// At the end of the game, a page is displayed with the final score and an input for user initials
-// The user intials and score are saved in local storage and displayed as the last saved input
-// There is a button to restart the quiz.
+
+
+// Timer 
+function startTimer() {
+    // setTime();      // This function controls how the time starts and when/if time gets added to it
+        interval = setInterval(function() {
+          timer--;
+          renderTime();
+        }, 1000);
+    }; 
+
+  // This function does 2 things. displays the time and checks to see if time is up.
+function renderTime() {
+    // When renderTime is called it sets the textContent for the timer html...;
+    timeDisplay.textContent = getFormattedSeconds();
+  
+   // ..and then checks to see if the time has run out
+    if (timer === 0) {
+        // Game ends. Take them to the high score page. 
+  
+      stopTimer();
+    }
+  };
+
+//   function stopTimer(){
+
+//   }
+
+//   function getFormattedSeconds(){
+
+//   }
+
+//   function setTime(){
+
+//   }
+
+// function correctAnswer {    // Code should show that they got it correct, add score points, and move to the next question. 
+
+// }
+
+// function wrongAnswer {      // Code should subtract time from the clock and move to the next question
+
+// }
